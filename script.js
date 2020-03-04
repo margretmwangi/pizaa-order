6fe9ed63a646a71c31ff8c76c863f45ad7aff778
margaret mwangi <margaret13mwangi@gmail.com>
2020-03-04 17:46:52 +0300

    modified script.js

--- script.js
+++ script.js
@@ -48,5 +48,22 @@ function Order(crust,size,sauce,topping){
         function submit() {
             alert();
           }
+           
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+       if (name === "") {
+      alert("please fill in your name");
+      return false;
+    } else if (email === "") {
+      alert("Please fill in your name and email");
+      return false;
+    } else {
+      alert(
+        name + ", We have received your message. Thank you for contacting   us."
+      );
+    }
+    event.preventDefault();
+  }
     
     
